72d15c69aabc7bfae2d45966fd4eaaf98dbd0a86
vaishnavanm5 <vaishnavanm5@gmail.com>
2023-08-04 21:30:58 +0530

    eslint and prettier configration

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
